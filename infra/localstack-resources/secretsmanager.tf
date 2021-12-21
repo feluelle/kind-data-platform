a7e98184fe435cf441b8414dc7612d0be839283e
Felix Uellendall <feluelle@pm.me>
2021-12-21 18:40:13 +0100

    Add aws_default connection

--- infra/localstack-resources/secretsmanager.tf
+++ infra/localstack-resources/secretsmanager.tf
@@ -1,3 +1,12 @@
+resource "aws_secretsmanager_secret" "airflow_connection_aws" {
+  name = "airflow/connections/aws_default"
+}
+
+resource "aws_secretsmanager_secret_version" "airflow_connection_aws" {
+  secret_id     = aws_secretsmanager_secret.airflow_connection_aws.id
+  secret_string = "aws://mock_access_key:mock_secret_key@:/?host=http%3A%2F%2Fhost.docker.internal%3A4566"
+}
+
 resource "aws_secretsmanager_secret" "airflow_connection_airbyte" {
   name = "airflow/connections/airbyte_default"
 }
