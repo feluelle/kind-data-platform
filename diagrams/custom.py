033e06f9ac874cf66dcb9d5b4367fc28c7352407
Felix Uellendall <feluelle@pm.me>
2021-12-22 17:18:31 +0100

    Refactor diagrams custom nodes

---
+++ diagrams/custom.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from os.path import splitext
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+
+from diagrams.custom import Custom
+
+
+@dataclass
+class HelperCustom:
+    """
+    Helps downloading the icon for the a custom node.
+
+    :param name: The name of the custom node.
+    :param url: The url to the icon to download.
+    :param file_extension: The file extension of the icon.
+    """
+
+    name: str
+    url: str
+    file_extension: str = ""
+
+    def download(self) -> None:
+        """
+        Downloads the icon from the url.
+        """
+        icon_path = self.get_icon_path()
+        urlretrieve(self.url, icon_path)
+
+    def get_icon_path(self) -> str:
+        """
+        Gets the local path to the icon.
+
+        :returns: the local path of the icon.
+        """
+        file_extension = self.file_extension or self._get_file_extension()
+        return f"./custom/{self.name}{file_extension}"
+
+    def _get_file_extension(self) -> str:
+        path = urlparse(self.url).path
+        return splitext(path)[1]
+
+
+class CustomSlim(Custom):
+    """
+    A slim version of the Custom node type which downloads the icon automatically.
+
+    :param name: The name of the custom node.
+    :param url: The url to the icon to download.
+    :param file_extension: The file extension of the icon.
+    """
+
+    def __init__(self, name: str, url: str, file_extension: str = ""):
+        node = HelperCustom(name, url, file_extension)
+        node.download()
+        super().__init__(label="", icon_path=node.get_icon_path())
+
+
+class Airbyte(CustomSlim):
+    """
+    FIXME: Use node from diagrams when available. See https://github.com/mingrammer/diagrams/issues/457 for more information.
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="airbyte",
+            url="https://assets.website-files.com/605e01bc25f7e19a82e74788/60895f8dfc189968c33b89d2_airbyte_rounded-rectangle_icon-p1myrdbsr61z7375uuocdxnlmab85b8vry3h73pfk0.png",
+        )
+
+
+class Dbt(CustomSlim):
+    """
+    Using a proper dbt icon.
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="dbt",
+            url="https://docs.google.com/uc?export=download&id=1fnsWHRu2a_UkJBJgkZtqt99x5bSyf3Aw",
+            file_extension=".png",
+        )
+
+
+class Superset(CustomSlim):
+    """
+    Using a proper superset icon.
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="superset",
+            url="https://www.apache.org/logos/res/superset/superset-2.png",
+        )
--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -1,42 +1,24 @@
-from urllib.request import urlretrieve
+from custom import Airbyte, Dbt, Superset
 
 from diagrams import Diagram
 from diagrams.aws.security import SecretsManager
 from diagrams.aws.storage import S3
-from diagrams.custom import Custom
 from diagrams.onprem.database import Postgresql
 from diagrams.onprem.monitoring import Grafana, Prometheus
 from diagrams.onprem.workflow import Airflow
-
-# FIXME: Use node from diagrams when available. See https://github.com/mingrammer/diagrams/issues/457 for more information.
-airbyte_url = "https://assets.website-files.com/605e01bc25f7e19a82e74788/60895f8dfc189968c33b89d2_airbyte_rounded-rectangle_icon-p1myrdbsr61z7375uuocdxnlmab85b8vry3h73pfk0.png"
-airbyte_icon = "./custom/airbyte.png"
-urlretrieve(airbyte_url, airbyte_icon)
-
-# Using a proper dbt icon
-dbt_url = (
-    "https://docs.google.com/uc?export=download&id=1fnsWHRu2a_UkJBJgkZtqt99x5bSyf3Aw"
-)
-dbt_icon = "./custom/dbt.png"
-urlretrieve(dbt_url, dbt_icon)
-
-# Using a proper superset icon
-superset_url = "https://www.apache.org/logos/res/superset/superset-2.png"
-superset_icon = "./custom/superset.png"
-urlretrieve(superset_url, superset_icon)
 
 with Diagram(
     name="kind-data-platform",
     show=False,
     graph_attr={"bgcolor": "transparent", "pad": "0.5"},
 ):
-    airbyte = Custom(label="", icon_path=airbyte_icon)
+    airbyte = Airbyte()
     airflow = Airflow()
-    dbt = Custom(label="", icon_path=dbt_icon)
+    dbt = Dbt()
     postgresql = Postgresql()
     grafana = Grafana()
     prometheus = Prometheus()
-    superset = Custom(label="", icon_path=superset_icon)
+    superset = Superset()
     s3 = S3()
     secretsmanager = SecretsManager()
 
