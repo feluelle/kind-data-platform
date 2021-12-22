2104bff51416a19ca80f811d803fc13a335019fe
Felix Uellendall <feluelle@pm.me>
2021-12-22 15:24:20 +0100

    Using proper logos for dbt and superset

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -4,7 +4,6 @@ from diagrams import Diagram
 from diagrams.aws.security import SecretsManager
 from diagrams.aws.storage import S3
 from diagrams.custom import Custom
-from diagrams.onprem.analytics import Dbt, Superset
 from diagrams.onprem.database import Postgresql
 from diagrams.onprem.monitoring import Grafana, Prometheus
 from diagrams.onprem.workflow import Airflow
@@ -14,6 +13,18 @@ airbyte_url = "https://assets.website-files.com/605e01bc25f7e19a82e74788/60895f8
 airbyte_icon = "./custom/airbyte.png"
 urlretrieve(airbyte_url, airbyte_icon)
 
+# Using a proper dbt icon
+dbt_url = (
+    "https://docs.google.com/uc?export=download&id=1fnsWHRu2a_UkJBJgkZtqt99x5bSyf3Aw"
+)
+dbt_icon = "./custom/dbt.png"
+urlretrieve(dbt_url, dbt_icon)
+
+# Using a proper superset icon
+superset_url = "https://www.apache.org/logos/res/superset/superset-2.png"
+superset_icon = "./custom/superset.png"
+urlretrieve(superset_url, superset_icon)
+
 with Diagram(
     name="kind-data-platform",
     show=False,
@@ -21,11 +32,11 @@ with Diagram(
 ):
     airbyte = Custom(label="", icon_path=airbyte_icon)
     airflow = Airflow()
-    dbt = Dbt()
+    dbt = Custom(label="", icon_path=dbt_icon)
     postgresql = Postgresql()
     grafana = Grafana()
     prometheus = Prometheus()
-    superset = Superset()
+    superset = Custom(label="", icon_path=superset_icon)
     s3 = S3()
     secretsmanager = SecretsManager()
 
